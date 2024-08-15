5959e3496ea8508da8c16c6e72ef4676ac25d336
Abdul Rahman <114587470+Rahman-Ar@users.noreply.github.com>
2024-08-15 07:46:12 +0000

    Pending changes exported from your codespace

---
+++ day1.py
@@ -0,0 +1,72 @@
+#printing hello world
+print("Hello, World!")
+
+#variable assignment
+
+a = 10
+b = 20.0
+c = "Abdul"
+d = True
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+#data types 
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+e = [1,2,3,4]
+f = (a,b,c,d)
+g = {"name":"Abdul","age":22}
+print(type(e))
+print(type(f))
+print(type(g))
+
+# arithmetic operator
+x = 10
+y = 3
+print("arithmetic operators")
+print(x + y) 
+print(x - y) 
+print(x * y) 
+print(x / y) 
+print(x // y) 
+print(x % y)  
+print(x ** y) 
+
+# Comparison operators
+print("Comparison operators")
+print(x > y)
+print(x < y)
+print(x == y)
+print(x != y)
+print(x >= y)
+print(x <= y)
+
+# logical operator
+
+x = True
+y = False
+print("Logical operators")
+print(x and y)
+print(x or y)
+print(not x)  
+
+#Experimenting with variables
+
+first_name = "Abdul"
+last_name = "Rahman"
+full_name = first_name + " " + last_name
+print(full_name)
+
+num = "100"
+num_int = int(num)
+print(num_int + 50)
+
+age = 22
+greeting = "Hello, I am " + full_name + " & I'm " + str(age) + " years old."
+print(greeting)
